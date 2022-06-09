84202d4b63f1008cb3af640ef2a6b0b6786da35c
rjurado01 <rjurado01@gmail.com>
2022-06-09 22:12:52 +0200

    Tutorial time: 1:06:00

---
+++ README.md
@@ -0,0 +1,5 @@
+Tutorial TypeScript con Node.js y Express. ¡Crea tu API REST con tipos estáticos DESDE CERO!
+
+[Video Link](https://www.youtube.com/watch?v=ZpY5KdGQvwI)
+
+By midudev
---
+++ src/routes/diaries.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import * as diaryServices from '../services/diariesService'
+
+const router = express.Router()
+
+router.get('/', (_req, res) => {
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
+
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(+req.params.id)
+
+  return (diary !== null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
+
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body
+
+  const newDiaryEntry = diaryServices.addDiary({
+    date,
+    weather,
+    visibility,
+    comment
+  })
+
+  res.send(newDiaryEntry)
+})
+
+export default router
